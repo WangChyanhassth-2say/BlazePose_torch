66c2e59514a7a2205a34bad662b58243c271791c
WangChyanhassth-2say <50102718+WangChyanhassth-2say@users.noreply.github.com>
2021-12-22 16:53:45 +0800

    Update BlazePose.py

--- BlazePose.py
+++ BlazePose.py
@@ -81,7 +81,7 @@ class BlazeBlock(nn.Module):
         self.depth_conv = nn.Conv2d(in_channels, in_channels, kernel_size=kernel_size, stride=stride,
                                     padding=padding, groups=in_channels)
         self.pointwise_conv = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0)
-        self.relu = nn.ReLU6(inplace=True)
+        self.relu = nn.ReLU(inplace=True)
 
     def forward(self, x):
         if self.use_pooling:
@@ -178,45 +178,18 @@ class BlazePose(nn.Module):
 
         # layers for the skip_connection
         self.conv7 = nn.Sequential(
-            nn.Conv2d(192, 288, 1, 1, 0, bias=False),
-            nn.BatchNorm2d(288),
-            nn.ReLU(),
-            nn.Conv2d(288, 288, 3, 1, 1, groups=288, bias=False),
-            nn.BatchNorm2d(288),
-            nn.ReLU(),
-            nn.Conv2d(288, 32, 1, 1, 0, bias=False),
-            nn.BatchNorm2d(32)
-            )
-        self.conv8 = nn.Sequential(
-            nn.Conv2d(128, 192, 1, 1, 0, bias=False),
-            nn.BatchNorm2d(192),
-            nn.ReLU(),
-            nn.Conv2d(192, 192, 3, 1, 1, groups=192, bias=False),
-            nn.BatchNorm2d(192),
-            nn.ReLU(),
             nn.Conv2d(192, 32, 1, 1, 0, bias=False),
             nn.BatchNorm2d(32)
             )
-        self.conv9 = nn.Sequential(
-            nn.Conv2d(64, 128, 1, 1, 0, bias=False),
-            nn.BatchNorm2d(128),
-            nn.ReLU(),
-            nn.Conv2d(128, 128, 3, 1, 1, groups=128, bias=False),
-            nn.BatchNorm2d(128),
-            nn.ReLU(),
+        self.conv8 = nn.Sequential(
             nn.Conv2d(128, 32, 1, 1, 0, bias=False),
             nn.BatchNorm2d(32)
             )
-        self.conv10 = nn.Sequential(
-            nn.Conv2d(32, 64, 1, 1, 0, bias=False),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.Conv2d(64, 64, 3, 1, 1, groups=64, bias=False),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
+        self.conv9 = nn.Sequential(
             nn.Conv2d(64, 32, 1, 1, 0, bias=False),
             nn.BatchNorm2d(32)
             )
+        # self.conv10 = nn.Sequential()
 
         # up sample layer
         self.upsample0 = nn.Upsample(scale_factor=2, mode='bilinear',align_corners=True)
@@ -229,13 +202,13 @@ class BlazePose(nn.Module):
             nn.Sigmoid()
             )
         self.conv14 = nn.Sequential(
-            nn.ReLU6(inplace=False),
+            nn.ReLU(inplace=False),
             nn.AdaptiveAvgPool2d((1, 1)),
             h_swish(),
             Reshape(192),
-            SwishLinear(192, 3*self.num_keypoints),
-            nn.Dropout(0.05),
-            Reshape(3, self.num_keypoints)
+            SwishLinear(192, 2*self.num_keypoints),
+            #nn.Dropout(0.05),
+            #Reshape(2, self.num_keypoints)
             )
         
     def forward(self, x):
@@ -260,15 +233,15 @@ class BlazePose(nn.Module):
         y3 = self.conv6_b1(y3)
         y3 = self.conv6_b2(y3)
 
-        # get heatmap
+        # get heat map
         x3 = self.conv7(y3)
         x2 = self.conv8(y2) + self.upsample2(x3)
         x1 = self.conv9(y1) + self.upsample1(x2)
-        x0 = self.conv10(y0) + self.upsample0(x1)
+        x0 = y0 + self.upsample0(x1)
         heatmap = self.conv11(x0) # => heatmap
-        # sorry I don't use offsetmap here as it in the paper
 
         # get joints
+       # with torch.no_grad():
         x = x0 + y0.detach()
         x = self.conv4_1(x) + y1.detach()
         x = self.conv5_1(x) + y2.detach()
@@ -283,7 +256,7 @@ class BlazePose(nn.Module):
             return heatmap
         else:
             return x
-        
+
     def _initialize_weights(self):
         # weight initialization
         for m in self.modules():
@@ -298,11 +271,10 @@ class BlazePose(nn.Module):
                 nn.init.normal_(m.weight, 0, 0.01)
                 if m.bias is not None:
                     nn.init.zeros_(m.bias)
-                    
-                    
+
 if __name__ == '__main__':
     from torchsummaryX import summary
 
     dummy_input = torch.rand(8,3,256,256)
-    model = BlazePose(33, 'two_head')
+    model = BlazePose(33, 'heatmap') # coco shape
     profile = summary(model,dummy_input)
